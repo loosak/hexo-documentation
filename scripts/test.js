a2a4f322e607ddf2d1be9db5edadb2c344317e85
loosak <kcoufal@gmail.com>
2017-01-11 14:18:34 +0100

    Docs updated: 2017-01-11 14:18:33)

--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,12 @@
-console.log("Hello test");
+console.log(`${new Date()} ... we are starting script!`);
+
+$(function(){
+			// --- versions
+			$('#version').text(`jQuery version: ${$().jquery}, Highcharts version: ${Highcharts.version}`);
+
+			function update() {
+					$('#date').text(new Date());
+			}
+
+			var myVar = setInterval( update, 1000);
+});
